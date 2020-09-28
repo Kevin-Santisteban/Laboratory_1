b99173e34346ee5af0ef112a35676a0f64c36a49
Kevin-Santisteban <34611023+Kevin-Santisteban@users.noreply.github.com>
2020-09-28 03:58:31 +0300

    Small fixes
    Changed value -0.4 to -4.0

--- Lab1.java
+++ Lab1.java
@@ -8,10 +8,10 @@ public static void main(String[] args) {
 //------------------1------------------------------
 long[] d = {4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
 
-//System.out.println(d.length);
+
 
 //------------------2------------------------------
-double Min_Range = -0.4;
+double Min_Range = -4.0;
 double Max_Range = 12.0;
 
 double[] x = new double[17]; 
