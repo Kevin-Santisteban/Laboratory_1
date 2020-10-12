2101104bbe677e9c59b68e8e89eff17a697e3ece
Kevin Santisteban <ksantisteban12@gmail.com>
2020-10-12 19:56:47 +0300

    Fixed indentation, added function for each Math Formula, Constants declared instead of using values.

--- Lab1.java
+++ Lab1.java
@@ -1,68 +1,98 @@
+public class Lab1 {
+    
+    
+    
+        public static void main(String[] args) {
+    
+    
+            //Initializing constants 
+    
+            final long[] d = {4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+            final double randomNumberMinRange = -4.0;
+            final double randomNumberMaxRange = 12.0;
+            final int width = 15;
+            final int height = 17;
+            final int[] tempArray = {5,7,11,12,13,15,16};
+    
+    
+    
+            //Filling array x with Random Numbers
+    
+            double[] x = new double[height];
+           
+            for (int k = 0; k < height; k++) {
+                x[k] = getRandomNumber(randomNumberMinRange, randomNumberMaxRange);
+            }
+    
+            //Initializing 2D array.  
+    
+            double[][] multidimensionalArray = new double[width][height];
+    
+    
+    
+            for (int i = 0; i < width; i++) {
+    
+                for (int j = 0; j < height; j++) {
+                    if (d[i] == 14) {
+                        
+                        multidimensionalArray[i][j] = firstMathFunction(x, j);
+                        	printFixedPointResult(multidimensionalArray[i][j]);
 
-public class Lab1{
+                    } else if (isInArray((int) d[i], tempArray)) {
+                        
+                        multidimensionalArray[i][j] = secondMathFunction(x, j);
+                        	printFixedPointResult(multidimensionalArray[i][j]);
 
+                    } else {
 
-public static void main(String[] args) {
-
-
-//------------------1------------------------------
-long[] d = {4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
-
-
-
-//------------------2------------------------------
-double Min_Range = -4.0;
-double Max_Range = 12.0;
-
-double[] x = new double[17]; 
-for (int k = 0; k < 17 ;k++) {
-	x[k]= ((double)Math.random() * (Max_Range - Min_Range )) + Min_Range;
-}
-
-//------------------3------------------------------
-
-double[][] DD = new double[15][17]; 
-int[] Temp_Array = {5,7,11,12,13,15,16};
-
-for (int i=0; i<15; i++) {
-	for (int j=0; j<17; j++) {
-	if (d[i] ==14) {
-			DD[i][j] = Math.atan(0.25 * ( 1/(Math.pow(Math.E, Math.abs(x[j])))));	
-				System.out.printf("%.4f ",(float)DD[i][j]);
-	
-		}else if (IsInArray((int)d[i], Temp_Array)) {
-				
-	DD[i][j] = Math.asin(Math.sin(Math.asin(Math.pow(Math.E,(-1)*Math.abs(x[j])))));			
-			System.out.printf("%.4f ",(float)DD[i][j]);
-		}else{
-			DD[i][j] = Math.pow(Math.pow(Math.E,( Math.cbrt(Math.tan(x[j]))/2 )),2);
-			System.out.printf("%.4f ",(float)DD[i][j]);
-}
-
-
-
-	
-}
-
-System.out.println("");
-	
-}
-
-
-
-}
-
-
-public static boolean IsInArray(int value ,int[] Array){
-
-	boolean result = false;
-		for (int i : Array ) {
-			if (i == value) {
-				result = true;
-			}
-		}
-return result;
-}
-
-
-}
+                        multidimensionalArray[i][j] = thirdMathFunction(x, j);
+                        	printFixedPointResult(multidimensionalArray[i][j]);
+                    }
+                }
+    
+                System.out.println("");
+            }
+    
+        }
+    
+    
+        public static boolean isInArray(int value, int[] Array) {
+            boolean result = false;
+            for (int i: Array) {
+                if (i == value) {
+                    result = true;
+                }
+            }
+            return result;
+        }
+    
+    
+        public static double firstMathFunction(double[] functionArray, int arrayIndex) {
+            return Math.atan(0.25 * (1 / (Math.pow(Math.E, Math.abs(functionArray[arrayIndex])))));
+    
+        }
+    
+    
+        public static double secondMathFunction(double[] functionArray, int arrayIndex) {
+            return Math.asin(Math.sin(Math.asin(Math.pow(Math.E, (-1) * Math.abs(functionArray[arrayIndex])))));
+    
+        }
+    
+    
+        public static double thirdMathFunction(double[] functionArray, int arrayIndex) {
+            return Math.pow(Math.pow(Math.E, (Math.cbrt(Math.tan(functionArray[arrayIndex])) / 2)), 2);
+    
+        }
+    
+    
+        public static void printFixedPointResult(double value) {
+    
+            System.out.printf("%.4f ", (float) value);
+        }
+    
+    
+    
+        public static double getRandomNumber(double minRange, double maxRange) {
+            return ((double) Math.random() * (minRange - maxRange)) + minRange;
+        }
+    }
